3dc95f8f4d32cf3229b0913020b57be7761a0245
oleksiichornii <alexprudent777@gmail.com>
2022-11-28 20:28:17 +0200

    changed modules

--- src/app/events-app.component.ts
+++ src/app/events-app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'events-app',
-  template: '<h2>Hello World</h2>',
+  template: '<h2>Hello World {{title}}</h2>',
 })
 export class EventsAppComponent {
-  title = 'angular15';
+  title = 'Events app';
 }
