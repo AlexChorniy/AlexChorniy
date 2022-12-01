06f7a2132c61dec825aaff5fe314c74e31bae804
oleksiichornii <alexprudent777@gmail.com>
2022-12-01 11:54:22 +0200

    added styles to navbar

--- src/app/nav/navbar.component.ts
+++ src/app/nav/navbar.component.ts
@@ -2,7 +2,22 @@ import {Component} from "@angular/core";
 
 @Component({
     templateUrl: './navbar.component.html',
-    selector: 'nav-bar'
+    selector: 'nav-bar',
+    styles: [`
+      .nav.navbar-nav {
+        font-size: 15px
+      }
+
+      #searchForm {
+        margin-left: 100px
+      }
+
+      @media (max-width: 1200px) {
+        #searchForm {
+          display: none
+        }
+      }
+    `]
 })
 export class NavbarComponent {
 
