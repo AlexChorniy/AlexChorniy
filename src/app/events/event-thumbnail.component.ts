d5d039963babad4b807355d2a79af6618962c703
oleksiichornii <alexprudent777@gmail.com>
2022-12-01 17:39:18 +0200

    using if example

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -6,7 +6,7 @@ import {Component, Input} from "@angular/core";
             <h2>{{event?.name}}</h2>
             <div>Date: {{event?.date}}</div>
             <div
-                    [ngClass]="{green: event?.time === '8:00 am', bold: event?.time === '8.00 am'}"
+                    [ngClass]="getStartTimeClass()"
                     [ngSwitch]="event?.time">
                 Time: {{event?.time}}
                 <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
@@ -49,4 +49,13 @@ import {Component, Input} from "@angular/core";
 
 export class EventThumbnailComponent {
     @Input() event: any;
+
+    getStartTimeClass() {
+        // const isEarlyStart = this.event && this.event.time === '8:00 am'
+        // return {green: isEarlyStart, bold: isEarlyStart}
+
+        if (this.event && this.event.time === '8:00 am')
+            return 'green bold'
+        return ''
+    }
 }
