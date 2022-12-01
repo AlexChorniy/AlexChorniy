0cb50e391bc9b84af2e8c763c6af09df77ce4f95
oleksiichornii <alexprudent777@gmail.com>
2022-12-01 17:57:15 +0200

    getStartTimeStyle():

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -6,7 +6,7 @@ import {Component, Input} from "@angular/core";
             <h2>{{event?.name}}</h2>
             <div>Date: {{event?.date}}</div>
             <div
-                    [style.color]="event?.time === '8:00 am' ? '#003300' : '#bbb'"
+                    [ngStyle]="getStartTimeStyle()"
                     [ngSwitch]="event?.time">
                 Time: {{event?.time}}
                 <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
@@ -24,14 +24,6 @@ import {Component, Input} from "@angular/core";
         </div>
     `,
     styles: [`
-      .green {
-        color: #003300 !important;
-      }
-
-      .bold {
-        font-weight: bold;
-      }
-
       .thumbnail {
         min-height: 210px
       }
@@ -57,5 +49,11 @@ export class EventThumbnailComponent {
         if (this.event && this.event.time === '8:00 am')
             return ['green', 'bold']
         return []
+    }
+
+    getStartTimeStyle(): any {
+        if (this.event && this.event.time === '8:00 am')
+            return {color: '#003300', 'font-weight': 'bold'}
+        return {}
     }
 }
