9c77526cdde8e8923fa7159da20c3d1f45d719c9
oleksiichornii <alexprudent777@gmail.com>
2022-12-07 10:34:31 +0200

    clean up the code, removing useless imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,20 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
-
 import {EventsAppComponent} from './events-app.component';
-import {EventsListComponent} from "./events/events-list.component";
-import {EventThumbnailComponent} from "./events/event-thumbnail.component";
+import {
+    CreateEventComponent,
+    EventDetailsComponent,
+    EventRouteActivatorService,
+    EventService,
+    EventsListComponent,
+    EventsListResolverService,
+    EventThumbnailComponent,
+} from './events';
 import {NavbarComponent} from "./nav/navbar.component";
-import {EventService} from "./events/shared/event.service";
 import {ToastrService} from "./common/toastr.service";
-import {EventDetailsComponent} from "./events/event-details/event-details.component";
 import {RouterModule, RouterOutlet} from "@angular/router";
 import {appRoutes} from "./routes";
-import {CreateEventComponent} from "./events/create-event.component";
 import {Error404Component} from "./errors/404.component";
-import {EventRouteActivatorService} from "./events/event-details/event-route-activator.service";
-import {EventsListResolverService} from "./events/events-list-resolver.service";
 
 @NgModule({
     imports: [
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,10 +1,12 @@
-import {EventsListComponent} from "./events/events-list.component";
-import {EventDetailsComponent} from "./events/event-details/event-details.component";
+import {
+    CreateEventComponent,
+    EventDetailsComponent,
+    EventRouteActivatorService,
+    EventsListComponent,
+    EventsListResolverService
+} from "./events";
 import {Routes} from "@angular/router";
-import {CreateEventComponent} from "./events/create-event.component";
 import {Error404Component} from "./errors/404.component";
-import {EventRouteActivatorService} from "./events/event-details/event-route-activator.service";
-import {EventsListResolverService} from "./events/events-list-resolver.service";
 
 export const appRoutes: Routes = [
     {path: 'events/new', component: CreateEventComponent, canDeactivate: ['canDeactivateCreateEvent']},
