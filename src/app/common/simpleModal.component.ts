b5eea46bbd1b7d25ad805afef10128d7329ef697
oleksiichornii <alexprudent777@gmail.com>
2023-01-19 12:26:35 +0200

    add closeOnBodyClick

--- src/app/common/simpleModal.component.ts
+++ src/app/common/simpleModal.component.ts
@@ -31,12 +31,15 @@ import {JQ_TOKEN} from "./jQuery.service";
 export class SimpleModalComponent {
     @Input() title: string = '';
     @Input() elementId: string = '';
+    @Input() closeOnBodyClick: string = 'false';
     @ViewChild('modalContainer') containerEl: ElementRef<HTMLInputElement> = {} as ElementRef;
 
     constructor(@Inject(JQ_TOKEN) private $: any) {
     }
 
     closeModal() {
-        this.$(this.containerEl.nativeElement).modal('hide');
+        if (this.closeOnBodyClick.toLocaleLowerCase() === 'true') {
+            this.$(this.containerEl.nativeElement).modal('hide');
+        }
     }
 }
