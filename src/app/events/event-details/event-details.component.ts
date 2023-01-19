45fcf8903992fe8b3a2e1e7f0ce040f408836cff
oleksiichornii <alexprudent777@gmail.com>
2023-01-19 11:50:05 +0200

    add routing to the same component

--- src/app/events/event-details/event-details.component.ts
+++ src/app/events/event-details/event-details.component.ts
@@ -1,6 +1,6 @@
 import {Component} from "@angular/core";
 import {EventService, IEvent, ISession} from "../shared";
-import {ActivatedRoute} from "@angular/router";
+import {ActivatedRoute, Params} from "@angular/router";
 
 @Component({
     templateUrl: './event-details.component.html',
@@ -26,7 +26,11 @@ export class EventDetailsComponent {
     }
 
     ngOnInit() {
-        this.event = this.eventService.getEvent(+this.route.snapshot.params['id'])
+        this.route.params.forEach((params: Params) => {
+            this.event = this.eventService.getEvent(+params['id']);
+            this.addMode = false;
+        })
+
     }
 
     addSession() {
